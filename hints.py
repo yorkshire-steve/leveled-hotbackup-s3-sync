07853f95ec48af05c91eba7f896ecff19d049539
Steve Wood <steve_wood16@hotmail.com>
2023-03-28 20:24:22 +0100

    add hints file support for typed buckets

--- hints.py
+++ hints.py
@@ -7,17 +7,29 @@ def create_hints_file(filename: str, journal_keys: list) -> None:
         with cdblib.Writer(f) as writer:
             for k in journal_keys:
                 sqn = k[0]
-                bucket = k[2][1].value
-                bkey = k[2][2].value
-                writer.putint(bucket + b"\x00" + bkey, sqn)
+                if type(k[2][1]) == tuple:
+                    buckettype = k[2][1][0].value
+                    bucket = k[2][1][1].value
+                    bkey = k[2][2].value
+                    writer.putint(buckettype + b"\x00" + bucket + b"\x00" + bkey, sqn)
+                else:
+                    bucket = k[2][1].value
+                    bkey = k[2][2].value
+                    writer.putint(bucket + b"\x00" + bkey, sqn)
 
-def get_sqn(filename: str, bucket: bytes, bkey: bytes) -> int:
+def get_sqn(filename: str, bucket: bytes, bkey: bytes, buckettype: bytes = None) -> int:
     with cdblib.Reader.from_file_path(filename) as reader:
-        return reader.getint(bucket + b"\x00" + bkey)
+        if buckettype:
+            return reader.getint(buckettype + b"\x00" + bucket + b"\x00" + bkey)
+        else:
+            return reader.getint(bucket + b"\x00" + bkey)
 
 if __name__ == "__main__":
     filename = sys.argv[1]
     bucket = sys.argv[2].encode('utf-8')
     bkey = sys.argv[3].encode('utf-8')
-    sqn = get_sqn(filename, bucket, bkey)
+    btype = None
+    if len(sys.argv) == 5:
+        btype = sys.argv[4].encode('utf-8')
+    sqn = get_sqn(filename, bucket, bkey, btype)
     print(sqn)
