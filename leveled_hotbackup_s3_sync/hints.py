f92e4b63fc69a2e411639cd91ed93337f6e7899d
Steve Wood <steve_wood16@hotmail.com>
2023-04-15 17:16:24 +0100

    change to poetry and add CICD

--- leveled_hotbackup_s3_sync/hints.py
+++ leveled_hotbackup_s3_sync/hints.py
@@ -1,13 +1,14 @@
-import sys
+from typing import Union
 
 import cdblib
 
+
 def create_hints_file(filename: str, journal_keys: list) -> None:
-    with open(filename, "wb") as f:
-        with cdblib.Writer(f) as writer:
+    with open(filename, "wb") as file_handle:
+        with cdblib.Writer(file_handle) as writer:
             for k in journal_keys:
                 sqn = k[0]
-                if type(k[2][1]) == tuple:
+                if isinstance(k[2][1], tuple):
                     buckettype = k[2][1][0].value
                     bucket = k[2][1][1].value
                     bkey = k[2][2].value
@@ -17,19 +18,9 @@ def create_hints_file(filename: str, journal_keys: list) -> None:
                     bkey = k[2][2].value
                     writer.putint(bucket + b"\x00" + bkey, sqn)
 
-def get_sqn(filename: str, bucket: bytes, bkey: bytes, buckettype: bytes = None) -> int:
+
+def get_sqn(filename: str, bucket: bytes, bkey: bytes, buckettype: Union[bytes, None] = None) -> int:
     with cdblib.Reader.from_file_path(filename) as reader:
         if buckettype:
             return reader.getint(buckettype + b"\x00" + bucket + b"\x00" + bkey)
-        else:
-            return reader.getint(bucket + b"\x00" + bkey)
-
-if __name__ == "__main__":
-    filename = sys.argv[1]
-    bucket = sys.argv[2].encode('utf-8')
-    bkey = sys.argv[3].encode('utf-8')
-    btype = None
-    if len(sys.argv) == 5:
-        btype = sys.argv[4].encode('utf-8')
-    sqn = get_sqn(filename, bucket, bkey, btype)
-    print(sqn)
+        return reader.getint(bucket + b"\x00" + bkey)
--- leveled_hotbackup_s3_sync/journal.py
+++ leveled_hotbackup_s3_sync/journal.py
@@ -1,19 +1,28 @@
 import cdblib
 import erlang
 
-from hints import create_hints_file
-from utils import swap_path, s3_path_exists, upload_file_to_s3, local_path_exists, ensure_parent_dir_exists, download_file_from_s3
+from leveled_hotbackup_s3_sync.hints import create_hints_file
+from leveled_hotbackup_s3_sync.utils import (
+    download_file_from_s3,
+    ensure_parent_dir_exists,
+    local_path_exists,
+    s3_path_exists,
+    swap_path,
+    upload_file_to_s3,
+)
+
 
 def list_keys(filename: str) -> list:
     with cdblib.Reader.from_file_path(filename) as reader:
         return [erlang.binary_to_term(x) for x in reader.keys()]
+
 
 def maybe_upload_journal(journal: tuple, source: str, destination: str, endpoint: str) -> None:
     journal_filename = f"{journal[1].decode('utf-8')}.cdb"
     hints_filename = f"{journal[1].decode('utf-8')}.hints.cdb"
     journal_s3_path = swap_path(journal_filename, source, destination)
     hints_s3_path = swap_path(hints_filename, source, destination)
-    
+
     if s3_path_exists(journal_s3_path, endpoint):
         print(f"{journal_s3_path} already exists")
     else:
@@ -26,6 +35,7 @@ def maybe_upload_journal(journal: tuple, source: str, destination: str, endpoint
         print(f"Uploading {journal_filename} to {journal_s3_path}")
         upload_file_to_s3(journal_filename, journal_s3_path, endpoint)
 
+
 def maybe_download_journal(journal: tuple, source: str, destination: str, endpoint: str) -> None:
     journal_filename = f"{journal[1].decode('utf-8')}.cdb"
     journal_local_path = swap_path(journal_filename, source, destination)
@@ -37,7 +47,8 @@ def maybe_download_journal(journal: tuple, source: str, destination: str, endpoi
         ensure_parent_dir_exists(journal_local_path)
         download_file_from_s3(journal_filename, journal_local_path, endpoint)
 
+
 def update_journal_filename(journal: tuple, source: str, destination: str) -> tuple:
-    current_journal_filename = journal[1].decode('utf-8')
+    current_journal_filename = journal[1].decode("utf-8")
     s3_path = swap_path(current_journal_filename, source, destination)
-    return (journal[0], s3_path.encode('utf-8'), journal[2], journal[3])
+    return (journal[0], s3_path.encode("utf-8"), journal[2], journal[3])
